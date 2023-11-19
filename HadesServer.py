0672b89d75e5e3679a5fe88b2c3f49c7e868cdff
Dan Lewis <48622577+danjlewis@users.noreply.github.com>
2023-11-19 15:48:00 +0000

    implemented read functionality for StyxScribeShared data

--- HadesServer.py
+++ HadesServer.py
@@ -5,13 +5,14 @@ import json
 import enum
 import dataclasses
 import uuid
+import os
 
 LOGGING_PREFIX = "HadesServer: "
 
 @enum.unique
 class IncomingMessageType(enum.Enum):
     REPL = enum.auto()
-    DATA = enum.auto()
+    READ_DATA = enum.auto()
 
 @dataclasses.dataclass
 class IncomingMessage:
@@ -40,7 +41,7 @@ class IncomingMessage:
         if "data" not in obj:
             obj["data"] = None
 
-        INCOMING_MESSAGE_TYPE_CODES = {"repl": IncomingMessageType.REPL, "data": IncomingMessageType.DATA}
+        INCOMING_MESSAGE_TYPE_CODES = {"repl": IncomingMessageType.REPL, "read_data": IncomingMessageType.READ_DATA}
         if obj["type"] not in INCOMING_MESSAGE_TYPE_CODES:
             return (None, {"error": "invalid `type` field value", "request_uuid": obj["request_uuid"]})
         msg_type = INCOMING_MESSAGE_TYPE_CODES[obj["type"]]
@@ -50,6 +51,7 @@ class IncomingMessage:
 @enum.unique
 class OutgoingMessageType(enum.Enum):
     REPL_OUTPUT = enum.auto()
+    DATA_OUTPUT = enum.auto()
     ERROR = enum.auto()
 
 @dataclasses.dataclass
@@ -60,51 +62,82 @@ class OutgoingMessage:
 
     def serialize(self):
         obj = dataclasses.asdict(self)
+        # obj = {"type": self.type, "data": self.data, "request_uuid": self.request_uuid}
 
-        OUTGOING_MESSAGE_TYPE_CODES = {OutgoingMessageType.REPL_OUTPUT: "repl_output", OutgoingMessageType.ERROR: "error"}
+        OUTGOING_MESSAGE_TYPE_CODES = {OutgoingMessageType.REPL_OUTPUT: "repl_output", OutgoingMessageType.DATA_OUTPUT: "data_output", OutgoingMessageType.ERROR: "error"}
         obj["type"] = OUTGOING_MESSAGE_TYPE_CODES[obj["type"]]
 
         obj = {k: v for k, v in obj.items() if v is not None}
 
         return json.dumps(obj)
 
-def run():
-    async def handler(socket):
-        print(LOGGING_PREFIX + "Client connected!")
+def convert_shared_data(obj):
+    if hasattr(obj, "_proxy"):
+        obj = getattr(obj, "_proxy")
 
-        async def hook(output):
-            if not socket.open:
-                return
+    if type(obj) in (list, tuple):
+        obj = [convert(x) for x in obj]
+        for x in obj:
+            print(type(x))
+    elif type(obj) == set:
+        obj = {convert(x): True for x in obj}
+        for k, v in obj.items():
+            print(type(k), type(v))
+    elif type(obj) == dict:
+        obj = {k: convert(v) for k, v in obj.items()}
+        for k, v in obj.items():
+            print(type(k), type(v))
 
-            request_uuid = None
-            if output.startswith("Request "):
-                output_split = output.split(": ")
-                request_uuid = output_split[0][8:]
-                output = "".join(output_split[1:])
+    return obj
 
-            await socket.send(OutgoingMessage(OutgoingMessageType.REPL_OUTPUT, output, request_uuid).serialize())
-        Scribe.AddHook(hook, "Response: ")
+async def handler(socket):
+    print(LOGGING_PREFIX + "Client connected!")
 
-        async for msg in socket:
-            msg, err = IncomingMessage.deserialize(msg)
+    async def hook(output):
+        if not socket.open:
+            return
 
-            if err is not None:
-                await socket.send(OutgoingMessage(OutgoingMessageType.ERROR, err["error"], err["request_uuid"]).serialize())
-                continue
+        request_uuid = None
+        if output.startswith("Request "):
+            output_split = output.split(": ")
+            request_uuid = output_split[0][8:]
+            output = "".join(output_split[1:])
 
-            if msg.type == IncomingMessageType.REPL:
+        await socket.send(OutgoingMessage(OutgoingMessageType.REPL_OUTPUT, output, request_uuid).serialize())
+    Scribe.AddHook(hook, "Response: ")
+
+    async for msg in socket:
+        msg, err = IncomingMessage.deserialize(msg)
+
+        if err is not None:
+            await socket.send(OutgoingMessage(OutgoingMessageType.ERROR, err["error"], err["request_uuid"]).serialize())
+            continue
+
+        match msg.type:
+            case IncomingMessageType.REPL:
                 if type(msg.data) != str:
                     await socket.send(OutgoingMessage(OutgoingMessageType.ERROR, "invalid `data` field type", msg.request_uuid).serialize())
                     continue
 
                 Scribe.Send(f"StyxScribeREPL: Request {msg.request_uuid}: {msg.data}")
+            case IncomingMessageType.READ_DATA:
+                if type(msg.data) != str:
+                    await socket.send(OutgoingMessage(OutgoingMessageType.ERROR, "invalid `data` field type", msg.request_uuid).serialize())
+                    continue
+                if msg.data not in Scribe.Modules.StyxScribeShared.Root:
+                    await socket.send(OutgoingMessage(OutgoingMessageType.ERROR, "unknown data key", msg.request_uuid).serialize())
+                    continue
 
-        print(LOGGING_PREFIX + "Client disconnected.")
+                data = convert_shared_data(Scribe.Modules.StyxScribeShared.Root[msg.data])
+                await socket.send(OutgoingMessage(OutgoingMessageType.DATA_OUTPUT, data, msg.request_uuid).serialize())
 
+    print(LOGGING_PREFIX + "Client disconnected.")
+
+def run():
     async def run():
-        PORT = 8000
-        async with websockets.serve(handler, "0.0.0.0", PORT):
-            print(f"Listening on port {PORT}...")
+        port = int(os.environ.get("PORT", "8000"))
+        async with websockets.serve(handler, "0.0.0.0", port):
+            print(LOGGING_PREFIX + f"Listening on port {port}...")
 
             await asyncio.Future()
     asyncio.run(run())
